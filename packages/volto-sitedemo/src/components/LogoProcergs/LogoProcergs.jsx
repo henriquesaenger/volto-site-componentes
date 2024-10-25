a5833e9a99f54285af15b7cb71d9aa45ba874146
blakolukas <lucasblacovicz@gmail.com>
2024-10-25 16:23:41 -0300

    footer inicial

--- packages/volto-sitedemo/src/components/LogoProcergs/LogoProcergs.jsx
+++ packages/volto-sitedemo/src/components/LogoProcergs/LogoProcergs.jsx
@@ -6,13 +6,15 @@ import { UniversalLink } from '@plone/volto/components';
 
 const LogoProcergs = () => {
   return (
-    <Container className="footer-barra-procergs">
+    <div className="footer-barra-procergs">
       <UniversalLink to="http://www.procergs.rs.gov.br/">
         <Image src={LogoImage} alt="Visite o site da PROCERGS" />
       </UniversalLink>
 
-      <Link to="/termos-de-uso">Termos de Uso</Link>
-    </Container>
+      <Link id="termos-de-uso" to="/termos-de-uso">
+        Termos de Uso
+      </Link>
+    </div>
   );
 };
 
--- packages/volto-sitedemo/src/components/SeloGoverno/SeloGoverno.jsx
+++ packages/volto-sitedemo/src/components/SeloGoverno/SeloGoverno.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { UniversalLink } from '@plone/volto/components';
-import { Container } from 'semantic-ui-react';
+import { div } from 'semantic-ui-react';
 
 const SeloGoverno = () => {
   return (
-    <Container className="footer-selo-governo">
+    <div className="footer-selo-governo">
       <UniversalLink href="https://www.estado.rs.gov.br" target="_self">
         <img
           src="https://www.estado.rs.gov.br/matriz_common/images/logos/marca_governo_RS.png"
           alt="RS.GOV - Novas façanhas"
           className="selo-governo--padrao"
         />
-        <img
-          src="https://www.estado.rs.gov.br/matriz_common/images/logos/marca_governo_RS_contraste.png"
-          alt="RS.GOV - Novas façanhas"
-          className="selo-governo--contraste"
-        />
       </UniversalLink>
-    </Container>
+    </div>
   );
 };
 
