557bdec6d889fb847844c956410f5e32e1f46386
samoel-silva <samoel-silva@procergs.rs.gov.br>
2024-11-13 13:43:45 -0300

    Modificação no footer

--- packages/volto-sitedemo/src/customizations/components/theme/Footer/Footer.jsx
+++ packages/volto-sitedemo/src/customizations/components/theme/Footer/Footer.jsx
@@ -4,18 +4,14 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import { useSelector, shallowEqual } from 'react-redux';
 import LogoProcergs from '../../../../components/LogoProcergs/LogoProcergs';
 import SeloGoverno from '../../../../components/SeloGoverno/SeloGoverno';
-import Endereco from '../../../../components/Endereco/Endereco';
+import { default as Sitemap } from '../../../../components/SiteMapFooter/SiteMapFooter';
 
 const Footer = ({ intl }) => {
-  const site = useSelector((state) => state.site.data);
   const navRoot = useSelector((state) => state.navroot?.data?.navroot);
-  const local = navRoot?.local?.data;
 
   return (
     <Segment id="footer" vertical padded inverted color="grey">
-      {site['plone.site_title']}
-
-      {local && <Endereco content={local} />}
+      <Sitemap location="" />
       {/* <SeloGoverno /> */}
       <LogoProcergs />
     </Segment>
