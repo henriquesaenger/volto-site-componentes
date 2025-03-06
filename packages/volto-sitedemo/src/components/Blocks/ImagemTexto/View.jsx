138727c88003d775403729d6041d7a329baa4a89
Fhabiana Machado <fhabiana-machado@procergs.rs.gov.br>
2025-03-06 16:34:37 -0300

    AB#1111129 - FIX: Linter

--- packages/volto-sitedemo/src/components/Blocks/ImagemTexto/View.jsx
+++ packages/volto-sitedemo/src/components/Blocks/ImagemTexto/View.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 const ImagemTextoBlockView = (props) => {
-  const { data, isEditMode, className } = props;
+  const { data } = props;
   const image_data = useMemo(() => data, [data]);
   const align =
     image_data?.image_position === 'right' || image_data?.image_position === ''
